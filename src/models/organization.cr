3ef08ce63332768c14bdbb35e95836627ad95988
Jack Thorne <jack@kilmer.co>
2022-12-31 14:01:01 -0600

    owner aliases

--- src/models/organization.cr
+++ src/models/organization.cr
@@ -1,6 +1,10 @@
 class Organization < BaseModel
   table do
     column name : String
-    belongs_to owner : User, foreign_key: :user_id
+    belongs_to owner : User # , foreign_key: :owner_id
+  end
+
+  def user_id
+    owner_id
   end
 end
--- src/operations/save_organization.cr
+++ src/operations/save_organization.cr
@@ -1,5 +1,7 @@
 class SaveOrganization < Organization::SaveOperation
-  # To save user provided params to the database, you must permit them
-  # https://luckyframework.org/guides/database/saving-records#perma-permitting-columns
-  permit_columns name
+  permit_columns name, owner_id
+
+  def user_id
+    owner_id
+  end
 end
